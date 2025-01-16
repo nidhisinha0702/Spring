5e67a92be646d08927984662e1adf6cbf2f414e3
Nidhi Kumari <nsinha7295@gmail.com>
2025-01-16 12:01:19 -0500

    day9

--- springorm/src/main/java/com/spring/orm/App.java
+++ springorm/src/main/java/com/spring/orm/App.java
@@ -1,5 +1,8 @@
 package com.spring.orm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,8 +17,50 @@ public class App {
         System.out.println("Hello World!");
         ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/orm/config.xml");
         StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
-        Student student = new Student(2324,"Nidhi Kumari","cincinnati");
-        int r = studentDao.insert(student);
-        System.out.println("done:::"+r);
+//        Student student = new Student(2324,"Nidhi Kumari","cincinnati");
+//        int r = studentDao.insert(student);
+//        System.out.println("done:::"+r);
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean go = true;
+        while(go) {
+        System.out.println("PRESS 1 for ass new student");
+        System.out.println("PRESS 2 for display all students");
+        System.out.println("PRESS 3 for get detail of single student");
+        System.out.println("PRESS 4 for delete student");
+        System.out.println("PRESS 5 for update student");
+        System.out.println("PRESS 6 for exit");
+        
+        try {
+        	int input = Integer.parseInt(br.readLine());
+        	switch(input) {
+        	case 1:
+        		//add a new student
+        		break;
+        	case 2:
+        		//display all student
+        		break;
+        	case 3:
+        		//get single student data
+        		break;
+        	case 4:
+        		//delete student
+        		break;
+        	case 5:
+        		//update student
+        		break;
+        	case 6:
+        		//exit
+        		go=false;
+        		break;
+        		
+        	}
+        	
+        }catch(Exception e) {
+        	System.out.println("Invalid input try with another one !!");
+        	System.out.println(e.getMessage());
+        }
+        }System.out.println("Thank you for using my application");
+        System.out.println("see you soon");
     }
 }
--- springorm/src/main/java/com/spring/orm/dao/StudentDao.java
+++ springorm/src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -18,7 +20,32 @@ public class StudentDao {
 		Integer i = (Integer)this.hibernateTemplate.save(student);
 		return i;
 	}
+	
+	//get the single data(object)
+	public Student getStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+	
+	//get all students(all rows)
+	public List<Student> getAllStudents(){
+		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
 
+	//delete
+	@Transactional
+	public void deleteStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+	}
+	
+	//update
+	@Transactional
+	public void updateStudent(Student student) {
+		this.hibernateTemplate.update(student);
+	}
+	
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
