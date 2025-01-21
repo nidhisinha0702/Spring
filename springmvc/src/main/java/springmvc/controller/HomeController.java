0aff60c7e97fb8254dbc5e57a118e163ac6d4ffb
Nidhi Kumari <nsinha7295@gmail.com>
2025-01-21 11:17:54 -0500

    day11

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,17 @@
 package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
-	public String home() {
+	@RequestMapping(path="/home",method = RequestMethod.GET)
+	public String home(Model model) {
 		System.out.println("this is home url");
+		model.addAttribute("name","Nidhi Kumari");
 		return "index";
 	}
 	
@@ -16,5 +19,11 @@ public class HomeController {
 	public String about() {
 		System.out.println("This is about");
 		return "about";
+	}
+	
+	@RequestMapping("/services")
+	public String services() {
+		System.out.println("This is services page");
+		return "services";
 	}
 }
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -10,5 +10,11 @@
 	<h1>This is home page</h1>
 	<h1>Called by home controller</h1>
 	<h1>url /home</h1>
+	
+	<%
+	 String name = (String) request.getAttribute("name");
+	%>
+	
+	<h1>Name is <%=name%></h1>
 </body>
 </html>
