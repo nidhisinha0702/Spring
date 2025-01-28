5015b122fb1754ed6deac6122530d1b78b975fdd
Nidhi Kumari <nsinha7295@gmail.com>
2025-01-28 17:35:22 -0500

    day14

--- springmvc/src/main/java/springmvc/controller/ContactController.java
+++ springmvc/src/main/java/springmvc/controller/ContactController.java
@@ -1,30 +1,55 @@
 package springmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import springmvc.model.User;
+import springmvc.service.UserService;
 
 @Controller
 public class ContactController {
 	
+	@Autowired
+	private UserService userService;
+	@ModelAttribute
+	public void commonDataForModel(Model m) {
+		m.addAttribute("Header","CodeWithNidhi");
+		m.addAttribute("Desc","Home for Programmer");
+		System.out.println("adding model data to view");
+	}
+	
 	@RequestMapping("/contact")
-	public String showForm() {
+	public String showForm(Model m) {
+		System.out.println("Creating form");
 		return "contact";
 	}
 	
 	@RequestMapping(path="/processform", method=RequestMethod.POST)
-	public String handleForm(@RequestParam("email") String userEmail, 
-			@RequestParam("userName") String userName, 
-			@RequestParam("userPassword") String userPassword, Model model) {
-		System.out.println("user email "+userEmail);
-		System.out.println("user name "+userName);
-		System.out.println("user password "+userPassword);
-		//process
-		model.addAttribute("name", userName);
-		model.addAttribute("email", userEmail);
-		model.addAttribute("password", userPassword);
+	public String handleForm(@ModelAttribute User user,Model model) {
+		System.out.println(user);
+		int createdUserId = this.userService.createUser(user);
+		model.addAttribute("msg", "User created with id "+createdUserId);
 		return "success";
 	}
+	
+	/*@RequestMapping(path="/processform", method=RequestMethod.POST)
+	public String handleForm(@RequestParam(name = "email",required=false) String userEmail, 
+			@RequestParam("userName") String userName, 
+			@RequestParam("userPassword") String userPassword, Model model) {
+		
+		User user = new User();
+		user.setEmail(userEmail);
+		user.setUserName(userName);
+		user.setUserPassword(userPassword);
+		
+		System.out.println(user);
+		//process
+		model.addAttribute("user",user);
+		
+		return "success";
+	}*/
 }
--- springmvc/src/main/webapp/WEB-INF/views/success.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/success.jsp
@@ -8,8 +8,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1> Welcome , ${name}</h1>
-<h1>Your email address is ${email}</h1>
-<h1>Your password is ${password} try to secure the password</h1>
+<h1>${Header}</h1>
+<p>${Desc}</p>
+<h1 style="color:green">${msg}</h1>
+<h1> Welcome , ${user.userName}</h1>
+<h1>Your email address is ${user.email}</h1>
+<h1>Your password is ${user.userPassword} try to secure the password</h1>
 </body>
 </html>
