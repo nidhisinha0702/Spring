0a40511dddd0234d9026102451e7baaf5f3a02f1
Nidhi Kumari <nsinha7295@gmail.com>
2025-03-27 12:55:57 -0400

    ems app

---
+++ ems-backendApp/src/main/java/codewithnidhi/ems_backendApp/EmsBackendAppApplication.java
@@ -0,0 +1,13 @@
+package codewithnidhi.ems_backendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendAppApplication.class, args);
+	}
+
+}
---
+++ ems-backendApp/src/test/java/codewithnidhi/ems_backendApp/EmsBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package codewithnidhi.ems_backendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
